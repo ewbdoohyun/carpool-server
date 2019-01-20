a247aedca15c4984ea9916377b90b49390459feb
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-01-20 20:41:21 +0900

    remove node_modules

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # carpool-server
 
-  based on GrapphQL, Typescript, NodeJS with Nomad Academy
+  based on GraphQL, Typescript, NodeJS with Nomad Academy
+
+
+
+
+yarn
+
+graphql-yoga
+helmet morgan cors
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("work");
+import { Options } from "graphql-yoga";
+import app from "./app";
+
+const PORT : number | string = process.env.PORT || 4000;
+const PLATGROUND_ENDPOINT : string = "/playground";
+const GRAPHQL_ENDPOINT : string = "/graphql";
+
+const appOptions : Options = {
+    port: PORT, 
+    playground: PLATGROUND_ENDPOINT,
+    endpoint : GRAPHQL_ENDPOINT
+}
+
+const handleAppStat = () => console.log(`Listing on port ${PORT}`)
+
+app.start(appOptions, handleAppStat);
