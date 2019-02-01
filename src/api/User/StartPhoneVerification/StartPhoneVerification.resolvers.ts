f9d69d347e135527151ec22e6c108a4c47164a05
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-02-01 21:53:07 +0900

    making phone resolvers

---
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -0,0 +1,29 @@
+import { Resolvers } from "src/types/resolvers";
+import { 
+    StartPhoneVerificationMutationArgs, 
+    StartPhoneVerificationResponse } from "src/types/graph";
+import Verification from "src/entities/Verification";
+
+
+const resolvers: Resolvers = {
+    Mutation: {
+        StartPhoneVerification: async (
+            _, 
+            args: StartPhoneVerificationMutationArgs
+            ) : Promise<StartPhoneVerificationResponse> => {
+                try{
+                    const { phoneNumber } = args;
+                    const existingVerification = await Verification.findOne({payload: phoneNumber});
+                    if(existingVerification){
+                        existingVerification.remove();
+                    }
+                }catch(error){
+                    return {
+                        ok: false,
+                        error: error.message
+                    }
+                }
+            }
+    }
+};
+export default resolvers;
