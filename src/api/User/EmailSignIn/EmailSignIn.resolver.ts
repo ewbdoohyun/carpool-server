e9bf764ff4ac5bfe6d5071a3d875c142bffa2eb7
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-02-01 21:27:53 +0900

    email Sign in

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolver.ts
@@ -0,0 +1,47 @@
+import { Resolvers } from "src/types/resolvers";
+import { EmailSignInMutationArgs, EmailSignInResponse } from "src/types/graph";
+import User from "src/entities/User";
+
+const resolvers: Resolvers = {
+    Mutation: {
+        EmailSignIn: async(
+            _, 
+            args:  EmailSignInMutationArgs
+            ) : Promise<EmailSignInResponse> => {
+            
+            const  {email,password} = args;
+            try{
+                const user = await User.findOne({email});
+                if(!user){
+                    return {
+                        ok: true,
+                        error: "No User with that Email",
+                        token: null
+                    }
+                }
+                const checkPassword = await user.comparePassword(password);
+                if(checkPassword){
+                    return{
+                        ok: true,
+                        error: null,
+                        token: "Comming soon"
+                    }
+                }else{
+                    return{
+                        ok: false,
+                        error: "Wrong password",
+                        token: null
+                    }
+                }
+            }catch(error){
+                return {
+                    ok: false,
+                    error: error.message,
+                    token: null
+                };
+            }
+        }
+    }
+};
+
+export default resolvers;
