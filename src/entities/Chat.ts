c7bb2921e9060a0aa4bd14183c969daa82140c6c
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-02-03 21:43:46 +0900

    debug

--- src/entities/Chat.ts
+++ src/entities/Chat.ts
@@ -1,30 +1,27 @@
-import { 
-    BaseEntity, 
-    Entity, 
-    PrimaryGeneratedColumn, 
-    CreateDateColumn,
-    OneToMany,
+import {
+  BaseEntity,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
 } from "typeorm";
+
 import Message from "./Message";
 import User from "./User";
 
-
 @Entity()
-class Chat extends BaseEntity{
-    @PrimaryGeneratedColumn() id: number;
+class Chat extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
 
-    @OneToMany(type => Message, message=>message.chat)
-    messages: Message[];
+  @OneToMany(type => Message, message => message.chat)
+  messages: Message[];
 
-    @OneToMany(type=> User, user=> user.chat)
-    participants: User[];
+  @OneToMany(type => User, user => user.chat)
+  participants: User[];
 
-    @CreateDateColumn() createdAt: string;
-    @CreateDateColumn() updatedAt: string;
+  @CreateDateColumn() createdAt: string;
 
-  
-    
+  @UpdateDateColumn() updatedAt: string;
 }
-
-
 export default Chat;
--- src/entities/Message.ts
+++ src/entities/Message.ts
@@ -1,35 +1,31 @@
-import { 
-    BaseEntity, 
-    Entity, 
-    PrimaryGeneratedColumn, 
-    CreateDateColumn,
-    ManyToOne,
-    Column
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
 } from "typeorm";
 
 import Chat from "./Chat";
 import User from "./User";
 
-
 @Entity()
-class Message extends BaseEntity{
-    @PrimaryGeneratedColumn() id: number
+class Message extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
 
-    @Column({type: "text"})
-    text: string;
+  @Column({ type: "text" })
+  text: string;
 
-    @ManyToOne(type =>Chat, chat=> chat.messages)
-    chat: Chat;
+  @ManyToOne(type => Chat, chat => chat.messages)
+  chat: Chat;
 
-    @ManyToOne(type => User, user => user.messages)
-    user: User;
+  @ManyToOne(type => User, user => user.messages)
+  user: User;
 
-    @CreateDateColumn() createdAt: string;
-    @CreateDateColumn() updatedAt: string;
+  @CreateDateColumn() createdAt: string;
 
-  
-    
+  @UpdateDateColumn() updatedAt: string;
 }
-
-
 export default Message;
