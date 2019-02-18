48f6af2c59b2195147240607f71f3e42d9224129
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-02-18 22:40:08 +0900

    UpdateRideStatus Resolver

---
+++ src/api/Ride/UpdateRideStatus/UpdateRideStatus.resolvers.ts
@@ -0,0 +1,64 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import { UpdateRideStatusMutationArgs, UpdateRideStatusRsponse } from "../../../types/graph";
+import Ride from "../../../entities/Ride";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    UpdateRideStatus: privateResolver(
+      async(
+        _, 
+        args: UpdateRideStatusMutationArgs, 
+        {req}
+      ) : Promise<UpdateRideStatusRsponse> => {
+      const user: User = req.user;
+      if(user.isDriving){
+        try{
+          let ride : Ride | undefined;
+          if(args.status === "ACCEPTED"){
+            const ride = await Ride.findOne({
+              id: args.rideId,
+              status: "REQUESTING"
+            });    
+            if(ride){
+              ride.driver = user;
+              user.isTaken = true;
+              user.save();
+            }
+          }else{
+            ride = await Ride.findOne({
+              id: args.rideId,
+              driver: user
+            });
+          }
+          if(ride){
+            ride.status = args.status
+            ride.save();
+            
+            return {
+              ok:true,
+              error: null
+            }
+          }else{
+            return{
+              ok: false,
+              error: "Can't update ride"
+            }
+          }
+        }catch(error){
+          return{
+            ok: false,
+            error: error.message
+          }
+        }
+      }else{
+        return{
+          ok: false,
+          error: "You are not driving"
+        }
+      }
+    })
+  }
+}
+export default resolvers;
