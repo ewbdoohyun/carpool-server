1146de2c83d50048fe2decc56b900e3fae1528e7
doohyunkim0304 <ewb.doohyun@gmail.com>
2019-04-29 23:26:11 +0900

    move database nas and change to maria

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -1,18 +1,36 @@
 import { ConnectionOptions } from "typeorm"
 
 const connectionOptions:ConnectionOptions = {
-    type: "postgres",
+    type: "mariadb",
     database: "nuber",
     synchronize: true,
     logging: true,
     entities: ["entities/*.*"],
-    host: process.env.DB_ENDPOINT,
-    port: 5433,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD 
+    host: process.env.MARIADB_ENDPOINT,
+    port: 3306,
+    username: process.env.MARIADB_USERNAME,
+    password: process.env.MARIADB_PASSWORD 
 }
 
 
 export default connectionOptions;
 
-// psql -U postgres -h localhost -W
+// // psql -U postgres -h localhost -W
+// import { ConnectionOptions } from "typeorm"
+
+// const connectionOptions:ConnectionOptions = {
+//     type: "postgres",
+//     database: "nuber",
+//     synchronize: true,
+//     logging: true,
+//     entities: ["entities/*.*"],
+//     host: process.env.DB_ENDPOINT,
+//     port: 5433,
+//     username: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD 
+// }
+
+
+// export default connectionOptions;
+
+// // psql -U postgres -h localhost -W
